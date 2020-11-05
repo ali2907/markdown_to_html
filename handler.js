5b75a91e3a636f1ab63520d4d058538c4abc9a2b
Ali Heidary <aliheidary@gmail.com>
2020-11-04 18:18:12 -0800

    handle system errors

--- handler.js
+++ handler.js
@@ -3,24 +3,30 @@
 const { convertToHtml } = require('./utils')
 
 module.exports.markdown_html_converter = (event, context, callback) => {
-    const inputMarkDown = event.body
+    try {
+        const inputMarkDown = event.body
+        const outputHtml = convertToHtml(inputMarkDown)
 
-    const outputHtml = convertToHtml(inputMarkDown)
+        const html = `<html><body>${outputHtml}</body></html>`
 
-    const html = `<html>
-                    <body>
-                      ${outputHtml}
-                    </body>
-                  </html>`
+        const response = {
+            statusCode: 200,
+            headers: {
+                'Content-Type': 'text/html',
+            },
+            body: html,
+        }
 
-    const response = {
-        statusCode: 200,
-        headers: {
-            'Content-Type': 'text/html',
-        },
-        body: html,
+        // callback is sending HTML back
+        callback(null, response)
+    } catch (err) {
+        console.log('Error: ', err)
+        callback(null, {
+            statusCode: 400,
+            headers: {
+                'Content-Type': 'text/html',
+            },
+            body: `<html><body><p>Sytem Error, please try again</p></body></html>`,
+        })
     }
-
-    // callback is sending HTML back
-    callback(null, response)
 }
